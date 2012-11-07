c8e7d21bf81824fe36bedef45182c5359ecb0703
Andy Barbour <andy.barbour@gmail.com>
2012-11-06 16:02:09 -0800

    struct linked to github:   https://github.com/abarbour/hydrogeo

--- R/func_cmsquared.R
+++ R/func_cmsquared.R
@@ -1,3 +1,4 @@
+#'  @
 cmsquared <- function(Perm_m2){
 	return(Perm_m2 * 1e4)
 }
---
+++ R/hydrogeo-package.R
@@ -0,0 +1,40 @@
+#' This package provides tools to calculate quantities commonly
+#' used in hydrogeology studies.
+#' 
+#' The main function to be used 
+#' is \code{\link{some_func}}
+#' 
+#' There are also two helper functions included: 
+#' \describe{
+#' \item{\code{\link{some_other_func}}}{ to do something.}
+#' }
+#'
+#' @section Scientific background:
+#'
+#' A bunch of stuff, and inline equation \eqn{r}, and a newline equation:
+#' \deqn{
+#' \frac{\partial^2 s}{\partial r^2}= 0
+#' }
+#' and some more.
+#' 
+#' And more.
+#'
+#' @docType package
+#' @name hydrogeo-package
+#' @aliases hydrogeo
+#' @title what this does
+#' 
+#' @author Andrew Barbour <andy.barbour@@gmail.com> 
+#' 
+# @import package
+#' 
+#' @references Hsieh, P. A., J. D. Bredehoeft, and J. M. Farr (1987),
+#' Determination of aquifer transmissivity from Earth tide analysis,
+#' \emph{Water Resour. Res.}, \strong{23} (10), 1824-1832, doi:10.1029/WR023i010p01824.
+#' 
+#' @references \url{http://www.agu.org/pubs/crossref/1987/WR023i010p01824.shtml}
+#'
+#' @seealso \code{\link{some_function}}, \code{\link{some_other_func}}
+#'  
+NULL
+#
