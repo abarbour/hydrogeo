bf0db9b642358f081b04d56cae17f5e26124f55d
Andy Barbour <abarbour@ucsd.edu>
2012-11-27 09:24:08 -0800

    documentation

--- R/func_kinvisc.R
+++ R/func_kinvisc.R
@@ -1,4 +1,4 @@
-#' function does this
+#' Calculate the kinematic viscosity
 #' 
 #' The main function to be used 
 #' is \code{\link{some_func}}
@@ -9,15 +9,16 @@
 #' }
 #'
 #'
-#' @name NAMEOFFUNC
+#' @name kinvisc
 #' export
 #'
+#' @param dvisc  scalar, representing the dynamic fluid viscosity, with units \eqn{[Pa \cdot s]}
+#' @param fdens  scalar, representing the fluid density, with units \eqn{[kg m^{-3}]}
+#' @param grav.divide  logical, should the kinematic viscosiy be divided by standard gravitational acceleration?
+#' @param verbose logical, should messages be displayed (\code{TRUE}), or not (\code{FALSE})?
 #'
-#' @param x  scalar, representing X with units \eqn{[m]}
-#'
-#' @return scalar, representing Y with units \eqn{[m]}
+#' @return scalar, a form of the kinematic viscosity, with units of either \eqn{[m^2 s^{-1}]} (if \code{grav.divide==FALSE}) or \eqn{[m \cdot s]} (if \code{grav.divide==TRUE})
 #' 
-#'
 #' @author Andrew Barbour <andy.barbour@@gmail.com> 
 #' 
 #' @references Hsieh, P. A., J. D. Bredehoeft, and J. M. Farr (1987),
@@ -35,18 +36,19 @@
 #' \dontrun{
 #' that
 #' }
-kinvisc <- function(dynamic_fluid_visc=1.002e-3, fluid_dens=1000, 
-	grav.divide=FALSE,
-	verbose=FALSE){
+kinvisc <- function(dvisc=1.002e-3, 
+                    fdens=1000, 
+                    grav.divide=FALSE,
+                    verbose=FALSE){
 	# kin visc defined as dynamic visc over density, in
 	# units of [m**2/s]
 	# divided by grav is [m**2 s**2/ s m] --> [m s]
-	mu. <- dynamic_fluid_visc
-	rho. <- fluid_dens
+	mu. <- dvisc
+	rho. <- fdens
 	nu. <- mu. / rho.
 	if (grav.divide){
-	  nu. <- nu./9.81
-	  if (verbose) message("kinem-visc. [L**2/T] divided by grav.; units are now [L*T]")
+	  nu. <- nu./9.80665
+	  if (verbose) message("kinem-visc. [m**2 / s] divided by grav.; units are now [m * s]")
 	}
 	return(nu.)
 }
