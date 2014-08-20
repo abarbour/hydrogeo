3d290a04dc65bf786dc326dd888e3eb5fc76c2e2
Andrew Barbour <abarbour@usgs.gov>
2014-08-20 12:54:22 -0700

    Added elastic properties and undrained response from seismic velocities, etc

---
+++ R/func_elasticprops.R
@@ -0,0 +1,54 @@
+#' Elastic properties from seismic velocities
+#' @name Elastic.Properties
+#' @export
+#' @aliases elasticProperties elastic_properties
+#' @param Vp numeric; the P-wave velocity of the rock in km/s
+#' @param Vs numeric; the S-wave velocity of the rock in km/s
+#' @param dens numeric; the rock density, in kg/m^3
+#' @param mu numeric; the shear modulus of the rock, in Pa
+Elastic.Properties <- function(Vp, Vs, dens){
+  mu <- shear_modulus(Vs, dens)
+  # undrained bulk mod
+  kap <- bulk_modulus(Vp=Vp, dens=dens, mu=mu)
+  comp <- 1/kap/1e9
+  data.frame(Mu.GPa=mu, Kappa.GPa=kap, Compress.per_Pa=comp)
+}
+
+#' @rdname Elastic.Properties
+#' @export
+shear_modulus <- function(Vs, dens){
+  dens * Vs * Vs
+}
+
+#' @rdname Elastic.Properties
+#' @export
+bulk_modulus <- function(Vp, Vs, dens, mu=NULL){
+  if (missing(Vs)){
+    if (is.null(mu)) stop("did not specify 'Vs' or 'mu'")
+  } else {
+    mu <- shear_modulus(Vs, dens)
+  }
+  dens * Vp * Vp  -  4 * mu / 3
+}
+
+#' Calculate undrained pressure response to volume strain
+#' @export
+#' @param mu numeric; the shear modulus of the rock, in Pa
+#' @param B numeric; Skempton's coefficient [0,1]
+#' @param nu_u numeric; the undrained Poisson's ratio
+#' @param as.areal logical; should the response be to areal strain
+#' rather than volume strain?  This assumes plane stress (a 
+#' traction free surface) and thus the form of vertical strain.
+Undrained.Response <- function(mu, B, nu_u=NULL, as.areal=FALSE){
+  #
+  chk0to1(B)
+  #
+  if (is.null(nu_u)){
+    const <- get_constants()
+    nu_u <- const$Poisson$nu_u
+  }
+  # from a volume strain (Roel 1996, Eq 5b, p146)
+  nurat <- (1 + nu_u)/(1 - 2*nu_u)
+  if (as.areal) nurat <- nurat * (1 - nu_u/(1-nu_u))
+  2 * mu * nurat * B / 3
+}
